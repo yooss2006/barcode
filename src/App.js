c979b12de175c5942a2259ea7a436703cfb933f6
yooss2006 <yooss2006@naver.com>
2022-09-06 14:08:14 +0900

    quagga 기본 셋팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import Scanner from "./scanner/Scanner";
+
 function App() {
-  return <div className="App">dd</div>;
+  const [carmera, setCarmera] = useState(false);
+  const [result, setResult] = useState(null);
+
+  const onClickToggleCarmera = () => {
+    setCarmera((prev) => !prev);
+  };
+
+  const onDetected = (result) => {
+    setResult(result);
+  };
+  return (
+    <div className="App">
+      <p>
+        결과: <span>{result}</span>
+      </p>
+      <button onClick={onClickToggleCarmera}>
+        {carmera ? "종료" : "시작"}
+      </button>
+      <div className="container">
+        {carmera && <Scanner onDetected={onDetected} />}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/scanner/Scanner.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import Quagga from "quagga";
+import config from "./config.json";
+
+const Scanner = ({ onDetected }) => {
+  const detected = (result) => {
+    onDetected(result.codeResult.code);
+  };
+
+  useEffect(() => {
+    Quagga.init(config, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      Quagga.start();
+      return () => {
+        Quagga.stop();
+      };
+    });
+
+    Quagga.onProcessed((result) => {
+      var drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(
+            0,
+            0,
+            Number(drawingCanvas.getAttribute("width")),
+            Number(drawingCanvas.getAttribute("height"))
+          );
+          result.boxes
+            .filter(function (box) {
+              return box !== result.box;
+            })
+            .forEach(function (box) {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                color: "green",
+                lineWidth: 2,
+              });
+            });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+            color: "#00F",
+            lineWidth: 2,
+          });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(
+            result.line,
+            { x: "x", y: "y" },
+            drawingCtx,
+            { color: "red", lineWidth: 3 }
+          );
+        }
+      }
+    });
+
+    Quagga.onDetected(detected);
+  }, []);
+
+  return (
+    // If you do not specify a target,
+    // QuaggaJS would look for an element that matches
+    // the CSS selector #interactive.viewport
+    <div id="interactive" className="viewport" />
+  );
+};
+
+export default Scanner;
